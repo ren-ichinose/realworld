237230712f4a968c0cdf4b7e3a38e3c454deda8b
Ren Ichinose <kamatani.toru@gmail.com>
2023-06-25 19:05:42 +0900

    fix_port-number

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(3005);
+  await app.listen(3000);
 }
 bootstrap();
